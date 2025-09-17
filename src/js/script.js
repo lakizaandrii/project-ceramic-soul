78a956013e0c209dccd7dd372573a6b4b9b3fe6c
lakizaandrii <lakizaandrii@gmail.com>
2025-09-17 13:17:34 +0200

    first commit

--- src/js/script.js
+++ src/js/script.js
@@ -122,7 +122,21 @@ try {
 					.querySelector("#checkbox")
 					.parentElement.parentElement.querySelector(".checkbox-error-message"),
 			}
-		);
+		)
+		.onSuccess((event) => {
+			const form = event.currentTarget;
+			const formData = new FormData(form);
+
+			fetch("https://httpbin.org/post", {
+				method: "POST",
+				body: formData,
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					console.log("Success", data);
+					form.reset();
+				});
+		});
 } catch (e) {}
 
 try {
